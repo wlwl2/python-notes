862ab592d57b147c1a5c8d60e957315186d024af
William Lim <manvinedi@gmail.com>
2017-12-17 08:23:29 +0800

    update

--- library-reference/python3lib.py
+++ library-reference/python3lib.py
@@ -31,7 +31,7 @@
 # Operation	# Result
 x or y	# if x is false, then y, else x
 x and y	# if x is false, then x, else y
-not x	# if x is false, then True, else False
+not x	# if x is false, then True, else False (highest priority)
 
 ## Comparisons
 
